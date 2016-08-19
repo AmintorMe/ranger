00d64684340bd3d12ff618e4fcbf6afc6aab319c
Amintor <amintor@tutanota.com>
2016-08-19 18:35:11 +0200

    added sha1 linemode

--- examples/plugin_linemode.py
+++ examples/plugin_linemode.py
@@ -9,10 +9,26 @@ import codecs
 import ranger.api
 from ranger.core.linemode import LinemodeBase
 
-
 @ranger.api.register_linemode
 class MyLinemode(LinemodeBase):
     name = "rot13"
-
     def filetitle(self, file, metadata):
         return codecs.encode(file.relative_path, "rot_13")
+
+@ranger.api.register_linemode
+class SHA1Linemode(LinemodeBase):
+    name="sha1"
+    def filetitle(self, file, metadata):
+        return file.relative_path
+    def infostring(self, file, metadata):
+        if file.is_directory:
+            return "DIR"
+        else:
+            import hashlib
+            hashed_file = open(file.path, "rb")
+            hash_object = hashlib.sha1()
+            hash_object.update(hashed_file.read())
+            hash_data=hash_object.hexdigest()
+            hashed_file.close()
+            return hash_data
+
